becc2a4c24f304512238f71e7bad776b5d0178d3
Heryk13 <otakiheryk@gmail.com>
2025-01-09 17:47:27 +0900

    prettier configurado

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,53 +1,55 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { FaPlus, FaRegTrashAlt } from "react-icons/fa";
+import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+import { FaPlus, FaRegTrashAlt } from 'react-icons/fa'
 
 export default function Home() {
   return (
     <div className="w-full">
-      <header className="flex justify-between p-3 bg-slate-400">
+      <header className="flex justify-between bg-slate-400 p-3">
         <p>2024/12/12</p>
-        <div>
-          aaaaaa
-        </div>
+        <div>aaaaaa</div>
       </header>
-      <Card className="w-[550px] h-[700px] mx-auto mt-5">
+      <Card className="mx-auto mt-5 h-[700px] w-[550px]">
         <CardContent>
-            <Card className="w-full mt-5">
-              <CardContent className="w-full py-3">
-                <div className="flex w-full justify-between h-full">
-                  <div>
-                    <p>2024/12/12</p>
-                    <h1><b>Grupo de amigos</b></h1>
-                    <p>grupo do projeto xxxxxx</p>
-                  </div>
-                  <div className="flex items-center">
-                    <FaRegTrashAlt size={50} />
-                  </div>
+          <Card className="mt-5 w-full">
+            <CardContent className="w-full py-3">
+              <div className="flex h-full w-full justify-between">
+                <div>
+                  <p>2024/12/12</p>
+                  <h1>
+                    <b>Grupo de amigos</b>
+                  </h1>
+                  <p>grupo do projeto xxxxxx</p>
                 </div>
-              </CardContent>
-            </Card>
-            <Card className="w-full mt-3">
-              <CardContent className="w-full py-3">
-                <div className="flex w-full justify-between">
-                  <div>
-                    <p>2024/12/12</p>
-                    <h1><b>Grupo de amigos</b></h1>
-                    <p>grupo do projeto xxxxxx</p>
-                  </div>
-                  <div className="flex items-center">
-                    <FaRegTrashAlt size={50} />
-                  </div>
+                <div className="flex items-center">
+                  <FaRegTrashAlt size={50} />
                 </div>
-              </CardContent>
-            </Card>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="mt-3 w-full">
+            <CardContent className="w-full py-3">
+              <div className="flex w-full justify-between">
+                <div>
+                  <p>2024/12/12</p>
+                  <h1>
+                    <b>Grupo de amigos</b>
+                  </h1>
+                  <p>grupo do projeto xxxxxx</p>
+                </div>
+                <div className="flex items-center">
+                  <FaRegTrashAlt size={50} />
+                </div>
+              </div>
+            </CardContent>
+          </Card>
         </CardContent>
       </Card>
-      <div className="w-full flex justify-center mt-10">
-        <Button className="w-[300px] h-[40px]" variant="outline">
+      <div className="mt-10 flex w-full justify-center">
+        <Button className="h-[40px] w-[300px]" variant="outline">
           <FaPlus />
         </Button>
       </div>
     </div>
-  );
+  )
 }
